5bc6cbefab32d7fcdf36e8b8e85c9315a1bd1c2a
Konstantin Baryshnikov <kbaryshnikov@upgate.co>
2021-12-21 00:03:55 +0300

    Changed config.update.url from github to gitlab

--- src/background/config.ts
+++ src/background/config.ts
@@ -2,7 +2,7 @@ import {AppConfig} from "./AppConfig";
 
 export const config: AppConfig = {
     update: {
-        url: 'https://inoblock.github.io/jsondb/config.json',
+        url: 'https://ovdinfo.gitlab.io/inoplugin/jsondb/config.json',
         periodMinutes: 60,
     },
 };
